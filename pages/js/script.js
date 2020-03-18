c15895437401fe3c8daf5bca5dfd338c5d8ce959
therarErifor <46041526+therarErifor@users.noreply.github.com>
2020-03-18 19:14:59 +0500

    Add files via upload

---
+++ pages/js/script.js
@@ -0,0 +1,57 @@
+"use strict"
+menu.onclick = function myFunction(){
+	var x = document.getElementById("myTopnav");
+
+	if(x.className ==="topnav"){
+		x.className += " responsive";
+	}	else {
+		x.className = "topnav";
+	}
+}
+
+$(document).ready(function () {
+          if (!$.browser.webkit) {
+              $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+          }
+      });
+
+function plus(){
+	let a, b, c;
+	a = document.getElementById('n1').value;
+	a = parseInt(a);
+
+	b = document.getElementById('n2').value;
+	b = parseInt(b);
+	c = +a + +b;
+	document.getElementById('out').value = c;
+}
+function minus(){
+	let a, b, d;
+	a = document.getElementById('n1').value;
+	a = parseInt(a);
+
+	b = document.getElementById('n2').value;
+	b = parseInt(b);
+	d = a - b;
+	document.getElementById('out').value = d;
+}
+function multiplication(){
+	let a, b, e;
+	a = document.getElementById('n1').value;
+	a = parseInt(a);
+
+	b = document.getElementById('n2').value;
+	b = parseInt(b);
+	e = a * b;
+	document.getElementById('out').value = e;
+}
+function division(){
+	let a, b, f;
+	a = document.getElementById('n1').value;
+	a = parseInt(a);
+
+	b = document.getElementById('n2').value;
+	b = parseInt(b);
+	f = a / b;
+	document.getElementById('out').value = f;
+}
