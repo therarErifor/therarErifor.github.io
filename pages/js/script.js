bc213599545a0a4b16dac897904388a6c65498e9
therarErifor <46041526+therarErifor@users.noreply.github.com>
2020-06-04 23:35:10 +0500

    суммури

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,19 +1,29 @@
 "use strict"
-menu.onclick = function myFunction(){
-	let x = document.getElementById("myTopnav");
 
-	if(x.className ==="topnav"){
-		x.className += " responsive";
-	}	else {
-		x.className = "topnav";
-	}
+//Get the button
+let mybutton;
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-$(document).ready(function () {
-          if (!$.browser.webkit) {
-              $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
-          }
-      });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	window.scrollTo({top: 0, behavior: 'smooth'});
+} 
+
+window.onload = function() { 
+	mybutton = document.querySelector("#back2Top");  
+};
+
+
 
 function plus(){
 	let a, b, c;
